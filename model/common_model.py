44f1dd84943ec3376fd8129c59b7e9be4713540c
jiangsha1007 <411040422@qq.com>
2019-06-18 13:02:26 +0800

    Update common_model.py

--- model/common_model.py
+++ model/common_model.py
@@ -16,7 +16,7 @@ class OsslibMetedata(SQLObject):
     community_id = StringCol(length=50,)
     community_from = IntCol(length=11)
     oss_name = StringCol(length=50)
-    oss_fullname = StringCol(length=100)
+    oss_fullname = StringCol(length=100, unique=True)
     oss_create_time = StringCol(length=50)
     oss_git_url = StringCol(length=200)
     oss_git_tool = StringCol(length=30)
